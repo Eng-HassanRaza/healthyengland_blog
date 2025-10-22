f8a9b204a3face2df7169e0b03f4f29bba38102a
Ubuntu <ubuntu@ip-172-31-20-166.ec2.internal>
2025-10-22 15:43:55 +0000

    smpt setting added

--- healthyengland/settings.py
+++ healthyengland/settings.py
@@ -173,18 +173,15 @@ SUMMERNOTE_CONFIG = {
 }
 
 # Email settings (for contact form)
-# For production, use SMTP backend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'healthyengland.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'info@healthyengland.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', '')  # Store password in environment variable
-DEFAULT_FROM_EMAIL = 'info@healthyengland.com'
-SERVER_EMAIL = 'info@healthyengland.com'
-
-# For development/testing, uncomment the line below:
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'info@healthyengland.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+SERVER_EMAIL = os.getenv('SERVER_EMAIL', EMAIL_HOST_USER)
 
 # Logging
 LOGGING = {
